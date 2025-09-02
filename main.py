f80167e0f8d3489cb46d5c1659e5000bfb1ae6b3
san43zko <san43zko@gmail.com>
2025-09-02 02:36:40 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,115 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import paramiko
+import os
+import re
+import a2s
+
+app=Flask(__name__)
+app.debug = True
+CORS(app)
+
+SSH_HOST = os.getenv('HOST_IP', "linfed.ru")
+SSH_USER = 'cs'
+SSH_KEY_PATH = 'C:/Users/SAN43Z/.ssh/id_ed25519'
+
+
+
+@app.route('/server-start', methods=['POST'])
+def start_server():
+    try:
+        data = request.get_json()
+
+        if not data or "id" not in data:
+            return jsonify({"error": "No id"}), 400
+
+        server_id = data["id"]
+
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(SSH_HOST, username=SSH_USER, key_filename=SSH_KEY_PATH)
+
+        stdin, stdout, stderr = ssh.exec_command(f'cs2-server @prac{server_id} start')
+        output = stdout.read().decode()
+        error = stderr.read().decode()
+
+        clean_output = re.sub(r'\x1b\[[0-9;]*m', '', output)
+        clean_output = re.sub(r'\*+\s*|\n\s*', ' ', clean_output).strip()
+
+        ssh.close()
+        return jsonify({"output": output, "error": error})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/server-stop', methods=['POST'])
+def stop_server():
+    try:
+        data = request.get_json()
+
+        if not data or "id" not in data:
+            return jsonify({"error": "No id"}), 400
+
+        server_id = data["id"]
+
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(SSH_HOST, username=SSH_USER, key_filename=SSH_KEY_PATH)
+
+        stdin, stdout, stderr = ssh.exec_command(f"cs2-server @prac{server_id} stop")
+        output = stdout.read().decode()
+        error = stderr.read().decode()
+
+        clean_output = re.sub(r'\x1b\[[0-9;]*m', '', output)
+        clean_output = re.sub(r'\*+\s*|\n\s*', ' ', clean_output).strip()
+
+        ssh.close()
+        return jsonify({"output": clean_output, "error": error})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/say', methods=['POST'])
+def say_mod():
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(SSH_HOST, username=SSH_USER, key_filename=SSH_KEY_PATH)
+
+        stdin, stdout, stderr = ssh.exec_command('cs2-server @prac3 exec say WORK!!!')
+        output = stdout.read().decode()
+        error = stderr.read().decode()
+
+        ssh.close()
+        return jsonify({"output": output, "error": error})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/status', methods=['POST'])
+async def check_status():
+    try:
+        data = request.get_json()
+        app.logger.info(f"Received data: {data}")
+
+        if not data or "serverIp" not in data or "serverPort" not in data:
+            app.logger.warning("Missing serverIp or serverPort in request")
+            return jsonify({"error": "No server ip or port"}), 400
+
+        server_ip = data['serverIp']
+        server_port = data['serverPort']
+        server_addres = (server_ip, server_port)
+
+        info = await a2s.ainfo(server_addres)
+        return jsonify({
+            "status": "online",
+            "players": str(info.player_count),
+            "map": info.map_name,
+            "port": info.port
+        })
+
+    except Exception as e:
+        return jsonify({
+            "status": "offline",
+            "error": str(e)
+        }), 500
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000)
